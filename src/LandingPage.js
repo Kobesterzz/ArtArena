c407ca7d0141b981b844f86474ac1a49829dcb57
kobe shaw <kobeshaw330@gmail.com>
2024-10-24 21:15:22 -0500

    working on landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,15 +1,21 @@
 // LandingPage.js
 import React from 'react';
 import './css/LandingPage.css';
+import lp from '/Users/kobesterzz/Documents/ArtArena/src/img/LP.jpg'
 
 function LandingPage({ navigateToPage }) {
   return (
     <div className="landing-page">
-      <h1>Welcome to Art Arena</h1>
-      <p>Discover amazing art, connect with artists, and share your creations.</p>
-      <div className="button-group">
-        <button onClick={() => navigateToPage('login')}>Log In</button>
-        <button onClick={() => navigateToPage('createAccount')}>Sign Up</button>
+      <div className="landingPageContent">
+        <img src={lp} class="bgIMG"></img>
+        <h1 className="intro">Welcome to <span className='title'>Art Arena</span></h1>
+        <div className="bottomContent">
+          <p>Discover amazing art, connect with artists, and share your creations.</p>
+          <div className="button-group">
+            <button onClick={() => navigateToPage('login')}>Log In</button>
+            <button onClick={() => navigateToPage('createAccount')}>Sign Up</button>
+          </div>
+        </div>
       </div>
     </div>
   );
