619a4fd7878328b35306811ff9f6966f03929ed7
kobe shaw <kobeshaw330@gmail.com>
2024-10-29 13:34:51 -0500

    working on the landing page animation

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,19 +1,38 @@
 // LandingPage.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import './css/LandingPage.css';
 import lp from '/Users/kobesterzz/Documents/ArtArena/src/img/LP.jpg';
 import { useNavigate } from 'react-router-dom';
+import anime from 'animejs';
+
 
 function LandingPage({ navigateToPage }) {
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
+
+   // Anime.js animation on mount
+   useEffect(() => {
+    anime({
+      targets: '.animated-text',
+      strokeDashoffset: [anime.setDashoffset, 0],
+      easing: 'easeInOutSine',
+      duration: 2000,
+      delay: 500,
+    });
+  }, []);
+
 
   const navigate = useNavigate();
 
   return (
     <div className="landing-page">
+      <div className="dim"></div>
       <div className="landingPageContent">
-        <div className="dim"></div>
         <img src={lp} className="bgIMG"></img>
-        <h1 className="intro">Welcome to <span className='title'>Art Arena</span></h1>
+        <svg viewBox="0 0 500 100" className="animated-text">
+          <text x="10" y="50" font-size="48">Welcome to Art Arena</text>
+        </svg>
+
         <div className="bottomContent">
           <p>Discover amazing art, connect with artists, and share your creations.</p>
           <div className="button-group">
